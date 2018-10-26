34a63c2268cf3eb3f65eeaf846f5ce790e56ad62
Daliborka Ciric <daliborka.ciric@propulsionapp.com>
2018-10-26 16:47:49 +0200

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+
+mongodb://bukicica:prodpass1@ds143293.mlab.com:43293/bukica-emaily-prod
