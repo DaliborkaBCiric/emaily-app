e3488df8e180e0bd34ead5757b86bc76e082ceab
daliborkaciric <daliborka.b.ciric@gmail.com>
2018-10-28 19:02:28 +0100

    added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent(){
@@ -11,15 +13,20 @@ class Header extends Component {
           <li><a href="/auth/google">Login with google</a></li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>
-
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: { this.props.auth.credits }
+          </li>,
+          <li key="3"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
   render(){
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Logo</a>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">Logo</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             {this.renderContent()}
           </ul>
--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
     googleClientID: '1078143330755-fn7ijupp8i24gkofk23kgmo46ienbfhh.apps.googleusercontent.com',
     googleClientSecret: 'c-j2R2CIARMFB7RDb8vJGqzH',
     mongoURI: 'mongodb://daliborka:bukicica89@ds141783.mlab.com:41783/bukica-emaily-dev',
-    cookieKey: 'slfsdlfjsdf;sdl;fksdlfksfdsjgihfgkfnvvskfs'
+    cookieKey: 'slfsdlfjsdf;sdl;fksdlfksfdsjgihfgkfnvvskfs',
+    stripePublishableKey: 'pk_test_s2vITVaS8wf0Ih1P6QMnmtxh',
+    stripeSecretKey: 'sk_test_gnBUR7LiR4wp8eg7Wb19P55I'
 };
