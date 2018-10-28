77e82ba30b1235091ed12ce6e63abdad8b1aed52
daliborkaciric <daliborka.b.ciric@gmail.com>
2018-10-28 19:32:39 +0100

    Another try to succesfully deploy

--- index.js
+++ index.js
@@ -30,12 +30,27 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('/client/build'));
+/**
+ * Production version verification. It needs to build for React client.
+ */
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file or main.css file!
+  // this guarantees that expressjs will look
+  // for specific files in specific directories
+  app.use(express.static('../client/build')); // if get request comes in from some route or some file or for anything for my application and we do not understant, then look in some file from this directory to see if some file that matches up the request
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route.
+  // If something arrives from a route that we do not understand,
+  // look at the index.htm file in the client directory
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    console.log ("NEXUS-__dirname: " + __dirname);
+    console.log ("NEXUS-res:" + res);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+}
 }
 
 const PORT = process.env.PORT || 5000;
