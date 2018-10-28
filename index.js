47b4f128a03e9230822fabe81f74b2d091668e5f
daliborkaciric <daliborka.b.ciric@gmail.com>
2018-10-28 19:48:21 +0100

    folder path changed

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('../client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    const index = path.join(__dirname, 'client', 'build', 'index.html');
+    res.sendFile(index);
   })
 }
 
